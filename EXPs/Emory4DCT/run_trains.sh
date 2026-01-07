98a250c7c3678a1bec7a553dcab86a7ea34e045b
Auzzer <haozhe@bu.edu>
2026-01-07 17:46:20 -0500

    1

--- EXPs/Emory4DCT/run_trains.sh
+++ EXPs/Emory4DCT/run_trains.sh
@@ -7,6 +7,6 @@ python GCN_SMS/train_gcn_sms.py --omega-u 0.5 --omega-alpha 0.5 --num-epochs 100
 # Run relative loss (normalized by ||u_obs||)
 python GCN_SMS/train_gcn_sms.py --omega-u 0.5 --omega-alpha 0.5 --num-epochs 100 > train_RelLoss.log 2>&1
 
-# Optional: print tail of each log after completion
+# print tail of each log after completion
 echo "Last lines of train_MSE.log:" && tail -n 20 train_MSE.log
 echo "Last lines of train_RelLoss.log:" && tail -n 20 train_RelLoss.log
